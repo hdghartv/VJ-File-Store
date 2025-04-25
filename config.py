e5e87398252c0a551b2b9824f5d27b8f26c8b3e1
hdghartv <148715573+hdghartv@users.noreply.github.com>
2025-04-26 02:16:20 +0500

    Update config.py

--- config.py
+++ config.py
@@ -20,7 +20,7 @@ def is_enabled(value, default):
 # Bot Information
 API_ID = int(environ.get("API_ID", "24558780"))
 API_HASH = environ.get("API_HASH", "bb6da015a0ee0ad279bc3b9381bfd1a0")
-BOT_TOKEN = environ.get("BOT_TOKEN", "6730469341:AAGsDq8PD9w6O7fr9EbQGXr2i4DEYwyTqtg")
+BOT_TOKEN = environ.get("BOT_TOKEN", "")
 
 PICS = (environ.get('PICS', 'https://graph.org/file/ce1723991756e48c35aa1.jpg')).split() # Bot Start Picture
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '5949508364').split()]
