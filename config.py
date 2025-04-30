b6811164c62093ec1f236e1bb67f918f0079add3
ajurani <148715573+ajurani@users.noreply.github.com>
2025-04-30 20:51:17 +0500

    Update config.py

--- config.py
+++ config.py
@@ -18,13 +18,13 @@ def is_enabled(value, default):
         return default
       
 # Bot Information
-API_ID = int(environ.get("API_ID", "24558780"))
-API_HASH = environ.get("API_HASH", "bb6da015a0ee0ad279bc3b9381bfd1a0")
+API_ID = int(environ.get("API_ID", "23365976"))
+API_HASH = environ.get("API_HASH", "3cad25149a717ba191e9f1c7a0dc5466")
 BOT_TOKEN = environ.get("BOT_TOKEN", "")
 
 PICS = (environ.get('PICS', 'https://graph.org/file/ce1723991756e48c35aa1.jpg')).split() # Bot Start Picture
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '6856545472').split()]
-BOT_USERNAME = environ.get("BOT_USERNAME", "Hdgharflix_bot") # without @
+BOT_USERNAME = environ.get("BOT_USERNAME", "filebotakibot") # without @
 PORT = environ.get("PORT", "8080")
 
 # Clone Info :-
